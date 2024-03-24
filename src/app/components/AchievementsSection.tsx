272c49e02cc2d646d3d2d5b5448f176d8aba4a23
Tushar Khatter <tusharkhatter2000@gmail.com>
2024-03-24 16:55:09 +0530

    fix: build break

--- src/app/components/AchievementsSection.tsx
+++ src/app/components/AchievementsSection.tsx
@@ -47,7 +47,7 @@ const AchievementsSection = () => {
                   animateToNumber={parseInt(achievement.value)}
                   locale="en-US"
                   className="text-white text-4xl font-bold"
-                  configs={(_, index) => {
+                  configs={(_: any, index: number) => {
                     return {
                       mass: 1,
                       friction: 100,
